a905e2a1afb6c5123155e7e04bc94743c9a30242
oyuyump <yuyumpoptec@gmail.com>
2024-08-13 22:09:49 -0600

    Agregar Login Fragment & activityPrincipal

---
+++ app/src/main/java/com/erha/androidbasic12/actividadfinal/PrincipalActivity.kt
@@ -0,0 +1,27 @@
+package com.erha.androidbasic12.actividadfinal
+
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.erha.androidbasic12.R
+import com.erha.androidbasic12.databinding.ActivityPrincipalBinding
+
+class PrincipalActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityPrincipalBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityPrincipalBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //Llamamos al fragment con el que inicia - Login
+        supportFragmentManager
+            .beginTransaction()
+            .add(R.id.fragmentPrincipal, LoginFragment.newInstance())
+            .commit()
+    }
+}
